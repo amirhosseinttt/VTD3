46acaaeb4e4600753d2feaab69574719ef84fdc3
Amirhossein <torkanlooa@gmail.com>
2025-02-18 17:49:29 +0330

    added botsort config

--- tracker/tracker.py
+++ tracker/tracker.py
@@ -1,26 +1,24 @@
-from ultralytics.trackers.bot_sort import BOTrack
+from ultralytics.trackers.bot_sort import BOTSORT
 import numpy as np
+import yaml
+from types import SimpleNamespace
 
 class Tracker:
     def __init__(self):
-        self.bot_sort = None
+        config = yaml.safe_load(open("config.yaml"))
+        args = SimpleNamespace(**config)
+        self.bot_sort = BOTSORT(args, frame_rate=30)
         self.frame_count = 0
 
-    def track(self, detection):
-        new_track = BOTrack(detection["tlwh"], detection["confidence"], detection["class"], feat=np.random.rand(128))
-        if self.bot_sort is None:
-            self.bot_sort = new_track
-            self.bot_sort.activate(kalman_filter=self.bot_sort.shared_kalman, frame_id=self.frame_count)
-            self.bot_sort.predict()
-        else:
-            self.bot_sort.update(new_track, frame_id=self.frame_count)
+    def track(self, dets, scores, cls, img=None):
+        tracks = self.bot_sort.init_track(dets, scores, cls, img)
+        self.bot_sort.multi_predict(tracks)
         
         self.frame_count += 1
-        print(self.bot_sort.track_id)
+        
 
 if __name__ == '__main__':
     tracker = Tracker()
-    detection = {"tlwh": np.array([0, 0, 10, 10, 0]), "confidence": 0.9, "class": 0}
-    tracker.track(detection)
-    
+    tracker.track(np.array([[1, 1, 2, 2, 0]]), np.array([0.9]), np.array([0]))
+    tracker.track(np.array([[1, 1, 2, 2, 0]]), np.array([0.9]), np.array([0]))
     
