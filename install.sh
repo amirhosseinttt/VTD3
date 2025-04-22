38979f7902a441ea75a56a281aff682e3d2a9e57
Amirhossein <torkanlooa@gmail.com>
2025-04-22 16:23:29 +0330

    installation pipeline completed

--- install.sh
+++ install.sh
@@ -6,4 +6,68 @@ if [ "$(id -u)" -ne 0 ]; then
     exit 1
 fi
 
+SCRIPTS_PATH=./scripts/
 
+
+
+# step-1: install docker and docker compose
+echo "Installing Docker..."
+filename=install_docker.sh
+if [ -f "$SCRIPTS_PATH$filename" ]; then
+    echo "Running $filename..."
+    bash "$SCRIPTS_PATH$filename"
+else
+    echo "$filename not found in $SCRIPTS_PATH"
+fi
+
+
+
+# step-2: change system DNS to shecan
+echo "Changing system DNS to Shecan..."
+filename=change_dns.sh
+if [ -f "$SCRIPTS_PATH$filename" ]; then
+    echo "Running $filename..."
+    bash "$SCRIPTS_PATH$filename"
+else
+    echo "$filename not found in $SCRIPTS_PATH"
+fi
+
+
+
+
+# step-3: create px4-gazebo-sitl docker image
+echo "Building px4-gazebo-sitl Docker image..."
+DOCKERFILE_NAME=PX4_Dockerfile
+IMAGE_NAME=px4-gazebo-sitl:latest
+filename=build_image.sh
+if [ -f "$SCRIPTS_PATH$filename" ]; then
+    echo "Running $filename..."
+    bash "$SCRIPTS_PATH$filename $SCRIPTS_PATH$DOCKERFILE_NAME $IMAGE_NAME"
+else
+    echo "$filename not found in $SCRIPTS_PATH"
+fi
+
+
+
+
+# step-4: create ROS2 docker image
+echo "Building ROS2 Docker image..."
+DOCKERFILE_NAME=ROS2_Dockerfile
+IMAGE_NAME=ros2:latest
+filename=build_image.sh
+if [ -f "$SCRIPTS_PATH$filename" ]; then
+    echo "Running $filename..."
+    bash "$SCRIPTS_PATH$filename $SCRIPTS_PATH$DOCKERFILE_NAME $IMAGE_NAME"
+else
+    echo "$filename not found in $SCRIPTS_PATH"
+fi
+
+
+# step-5: start the docker compose service
+echo "Starting Docker Compose service..."
+docker-compose up 
+
+
+
+# final-step
+echo "Installation completed successfully!"
